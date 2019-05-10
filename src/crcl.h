546f3cfa95ca866f3b039e7798e3500135c86d45
Nichidanil <nichi.par@mail.ru>
2019-05-10 14:50:05 +0700

    Add crcl.h

---
+++ src/crcl.h
@@ -0,0 +1,17 @@
+#ifndef CRCL_H
+#define CRCL_H
+
+#define N 200
+
+typedef struct {
+    double x;
+    double y;
+    double r;
+    int ind;
+} Crcl;
+
+void circle(Crcl* in, int col);
+int start(char* c, Crcl* in, int ind);
+double perimetr(Crcl* in, int i);
+double area(Crcl* in, int i);
+#endif
